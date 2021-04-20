03165af2589b947f2f7057c3a8b5fcff243cab88
vanek-filkin <vanek.filkin@yandex.ru>
2021-04-20 12:06:02 +0000

    first_model

--- web/mysite/mysite/settings.py
+++ web/mysite/mysite/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'products',
     'django.contrib.admin',
-    'products.apps',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
--- web/mysite/products/models.py
+++ web/mysite/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    type_of = models.CharField(max_length=200)
+    manufacturer = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.FloatField()
+    rel_date = models.DateTimeField()
