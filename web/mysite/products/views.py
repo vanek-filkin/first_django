dfc8b1539857967fa33a7e9c9a8b99831b20bc90
vanek-filkin <vanek.filkin@yandex.ru>
2021-04-21 09:21:58 +0000

    post_data

--- web/mysite/products/views.py
+++ web/mysite/products/views.py
@@ -12,9 +12,12 @@ def detail(request, prod_id):
     return render(request, 'products/detail.html', {'product': product})
 
 def add_new(request):
+    if request.method=="POST":
+        q = Product(*data)
     return render(request, 'products/new_product.html')
 
 def delete(request,prod_id):
+    Product.objects.filter(id=prod_id).delete()
     new_prod_list = Product.objects.order_by('-rel_date')
     context = {'new_prod_list': new_prod_list}
     return render(request, 'products/index.html', context)
