8d058c016d34e08cb9214fb573c2b729a23ae3bb
vanek-filkin <vanek.filkin@yandex.ru>
2021-04-27 12:42:36 +0000

    ready for see

--- web/mysite/products/models.py
+++ web/mysite/products/models.py
@@ -4,6 +4,7 @@ import datetime
 
 # Create your models here.
 class Product(models.Model):
+
     name = models.CharField(max_length=200)
     type_of = models.CharField(max_length=200)
     manufacturer = models.CharField(max_length=200)
@@ -11,7 +12,14 @@ class Product(models.Model):
     description = models.TextField()
     price = models.FloatField()
     rel_date = models.DateTimeField()
+    img = models.ImageField(upload_to='images/' )
+
     def __str__(self):
-        return self.name;
+        result = (self.name + '\n' + self.type_of + self. manufacturer + '\n' +
+        self.country + '\n' + self.description + '\n' + str(self.price) + '\n' +
+        str(self.rel_date.year))
+
+        return result
+
     def was_release_recently(self):
         return self.rel_date >= timezone.now() - datetime.timedelta(days=1)
--- web/mysite/products/views.py
+++ web/mysite/products/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Product
+from django.http import HttpResponseRedirect
+from django.core.files.storage import FileSystemStorage
+
 # Create your
 
 def index(request):
@@ -12,12 +15,25 @@ def detail(request, prod_id):
     return render(request, 'products/detail.html', {'product': product})
 
 def add_new(request):
-    if request.method=="POST":
-        q = Product(*data)
-    return render(request, 'products/new_product.html')
+    if request.method == "POST":
+        if request.FILES:
+            file = request.FILES['photo']
+            fs = FileSystemStorage()
+            filename = fs.save(file.name, file)
+        data = request.POST
+        new_data = Product(name = data['name'],
+                           type_of = data['category'],
+                           manufacturer = data['manufacturer'],
+                           country = data['country'],
+                           description = data['description'],
+                           price = int(data['price']),
+                           rel_date = data['rel_date'],
+                           img = request.FILES['photo']
+                            )
+        new_data.save()
+        return HttpResponseRedirect('..')
+    return render(request,'products/new_product.html')
 
 def delete(request,prod_id):
     Product.objects.filter(id=prod_id).delete()
-    new_prod_list = Product.objects.order_by('-rel_date')
-    context = {'new_prod_list': new_prod_list}
-    return render(request, 'products/index.html', context)
+    return HttpResponseRedirect('../..')
